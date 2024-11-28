1c601cfe666bf097217e58cc710b6fd0f8de3706
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-28 12:28:24 +0530

    rest_api_created

---
+++ restapi_pro/rest_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+class Sample(serializers.Serializer):
+    name=serializers.CharField()
+    age=serializers.IntegerField()
+    email=serializers.EmailField()
+    place=serializers.CharField()
+
+class model_serializer(serializers.ModelSerializer) :
+    class Meta  :
+        model=User
+        fields='__all__'
